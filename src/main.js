cdd2b84118b5fac65b7167dcab3e216a8db59990
Liyong <763739903@qq.com>
2019-11-29 11:42:52 +0800

    引用注册element-ui组件完成

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 导入element-ui组件
+import ElementUI from 'element-ui'
+// 导入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册elementUI
 new Vue({
   router,
   render: h => h(App)
